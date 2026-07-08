df5f6b3897afd4855456b13d87d63f4cab2e4bd7
Nathan Cherny <natec3632@gmail.com>
2026-07-08 17:53:22 -0400

    wordpress

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-16.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"79e85df9","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSiteKeyV3":"","afterSubmitToEmail":"","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-79e85df9', 16 );
+});
+ });
